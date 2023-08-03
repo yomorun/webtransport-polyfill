6fc55bf90a17388eacfbd0a149f00fbb3a4c9ab2
Noah Lu <15753140326@163.com>
2023-08-03 22:32:09 +0800

    feat: close event

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 // import "regenerator-runtime/runtime.js";
-import { BidirectionalStream } from './BidirectionalStream';
-import { UnidirectionalStream } from './UnidirectionalStream'
-import { Datagrams } from './Datagrams';
-import { WebTransportCloseInfo } from './CloseInfo';
-import { ServerInitiatedStreams } from './ServerInitiatedStreams';
+import { BidirectionalStream } from "./BidirectionalStream";
+import { UnidirectionalStream } from "./UnidirectionalStream";
+import { Datagrams } from "./Datagrams";
+import { WebTransportCloseInfo } from "./CloseInfo";
+import { ServerInitiatedStreams } from "./ServerInitiatedStreams";
 // import { ReceiveStream } from './ReceiveStream';
 // import { SendStream } from './SendStream';
 
@@ -19,60 +19,69 @@ export class WebTransportPolyfill {
   public ready: Promise<unknown>;
   public close: (closeInfo?: WebTransportCloseInfo) => void;
   datagrams: Datagrams | null = null;
-  public incomingBidirectionalStreams: ServerInitiatedStreams = new ServerInitiatedStreams()
-  public incomingUnidirectionalStreams: ServerInitiatedStreams = new ServerInitiatedStreams()
+  public incomingBidirectionalStreams: ServerInitiatedStreams =
+    new ServerInitiatedStreams();
+  public incomingUnidirectionalStreams: ServerInitiatedStreams =
+    new ServerInitiatedStreams();
   #ws: WebSocket | null = null;
   #connErr: any;
 
   // https://www.w3.org/TR/webtransport/#webtransport-constructor
   // constructor(USVString url, optional WebTransportOptions options = {});
   constructor(_url: string) {
-    let url: URL
+    let url: URL;
     try {
       url = new URL(_url);
-      if (url.protocol !== 'https:') {
+      if (url.protocol !== "https:") {
         // 5.2.3 If parsedURL scheme is not https, throw a SyntaxError exception.
         // be careful, do not allow `wss` protocol here, this prevents code reuseable when upgrade to webtransport. otherwise, developer need to change `wss` to `https` when upgrade from websocket to webtransport.
-        throw new SyntaxError("Invalid protocol")
+        throw new SyntaxError("Invalid protocol");
       }
-      if (url.hash !== '') {
+      if (url.hash !== "") {
         // 5.2.4 If parsedURL fragment is not null, throw a SyntaxError exception.
-        throw new SyntaxError("Fragment is not permitted")
+        throw new SyntaxError("Fragment is not permitted");
       }
     } catch (err) {
       // 5.2.2 If parsedURL is a failure, throw a SyntaxError exception
-      throw new SyntaxError(err.message)
+      throw new SyntaxError(err.message);
     }
     // change `https` to `wss`
-    url.protocol = 'wss';
+    url.protocol = "wss";
     let parsedUrl = url.toString();
 
     this.#ws = new WebSocket(parsedUrl);
-    this.#ws.binaryType = 'arraybuffer';
+    this.#ws.binaryType = "arraybuffer";
 
-    console.info("%cWebTransport polyfilled", "color: white; background-color: green");
+    console.info(
+      "%cWebTransport polyfilled",
+      "color: white; background-color: green"
+    );
+
+    this.#ws.onclose = (event) => {
+      console.log("ws close", event);
+    };
 
     // readonly attribute Promise<WebTransportCloseInfo> closed;
     this.closed = new Promise((resolve, reject) => {
       if (!this.#ws) {
-        return reject(Error('WebTransport is closed'));
+        return reject(Error("WebTransport is closed"));
       }
-      this.#ws.addEventListener('close', (ce) => {
-        resolve(ce)
+      this.#ws.addEventListener("close", (ce) => {
+        resolve(ce);
       });
     });
 
     // readonly attribute Promise<undefined> ready;
     this.ready = new Promise((resolve, reject) => {
       if (!this.#ws) {
-        return reject(Error('WebTransport is closed'));
+        return reject(Error("WebTransport is closed"));
       }
 
-      this.#ws.addEventListener('open', () => {
+      this.#ws.addEventListener("open", () => {
         resolve(null);
       });
-      this.#ws.addEventListener('error', (err) => {
-        console.log(err, 'error')
+      this.#ws.addEventListener("error", (err) => {
+        console.log(err, "error");
         this.#connErr = err;
         reject(err);
       });
@@ -89,17 +98,17 @@ export class WebTransportPolyfill {
       // ref: https://www.rfc-editor.org/rfc/rfc6455.html#section-7.4.2
       if (closeInfo) {
         if (closeInfo.closeCode < 3000 || closeInfo.closeCode > 4999) {
-          closeInfo.closeCode = 4000
+          closeInfo.closeCode = 4000;
         }
       }
       this.#ws.close(closeInfo?.closeCode, closeInfo?.reason);
-    }
+    };
   }
 
   // Promise<WebTransportBidirectionalStream> createBidirectionalStream(optional WebTransportSendStreamOptions options = {});
   createBidirectionalStream(): Promise<BidirectionalStream> {
     return new Promise((resolve, reject) => {
-      if (!this.#ws) return reject(Error('WebTransport is closed'));
+      if (!this.#ws) return reject(Error("WebTransport is closed"));
       resolve(new BidirectionalStream(this.#ws));
     });
   }
@@ -107,7 +116,7 @@ export class WebTransportPolyfill {
   // Promise<WebTransportUniidirectionalStream> createBidirectionalStream(optional WebTransportSendStreamOptions options = {});
   createUnidirectionalStream(): Promise<UnidirectionalStream> {
     return new Promise((resolve, reject) => {
-      if (!this.#ws) return reject(Error('WebTransport is closed'));
+      if (!this.#ws) return reject(Error("WebTransport is closed"));
       resolve(new UnidirectionalStream(this.#ws));
     });
   }
@@ -128,8 +137,8 @@ export class WebTransportPolyfill {
   // }
 }
 
-if (typeof window !== 'undefined') {
-  if (typeof window.WebTransport === 'undefined') {
+if (typeof window !== "undefined") {
+  if (typeof window.WebTransport === "undefined") {
     window.WebTransport = WebTransportPolyfill;
   }
 }
